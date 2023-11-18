713e5b56a52f98a7a28aa3c64417aea62c7b928f
mohammadne <mohammadne.dev@gmail.com>
2023-11-18 16:02:34 +0330

    Add webhook

--- main.go
+++ main.go
@@ -96,6 +96,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Executer")
 		os.Exit(1)
 	}
+	if err = (&appsv1alpha1.Executer{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Executer")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
